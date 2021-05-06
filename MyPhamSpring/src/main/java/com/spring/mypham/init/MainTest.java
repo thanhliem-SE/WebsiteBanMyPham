368cbbf8d4066f1898a37722a7a700c55a268add
Admin <thanhliem2n@gmail.com>
2021-05-06 21:41:21 +0700

    a

--- MyPhamSpring/src/main/java/com/spring/mypham/init/MainTest.java
+++
@@ -1,9 +0,0 @@
-package com.spring.mypham.init;
-
-import com.spring.mypham.dao.MySessionFactorys;
-
-public class MainTest {
-	public static void main(String[] args) {
-		MySessionFactorys.getInstance().getSessionFactory();
-	}
-}
